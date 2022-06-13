787f7f6d8aa11d0f1a2c27ecdbf8565e32d6dc39
mdelc <chemandelcarpio@yahoo.com>
2022-06-12 18:08:57 -0700

    completed week 4 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -13,37 +14,102 @@
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
+
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+describe("shuffle", () => {
+   const colors1 = ["purple", "blue", "green", "yellow", "pink"] // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"] // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+    it("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+        expect(shuffle(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+        expect(shuffle(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+    }) 
+})
+
+// ● shuffle › takes in an array, removes the first item from the array and shuffles the remaining content
+
+// ReferenceError: shuffle is not defined
 
 
 // b) Create the function that makes the test pass.
+
+
+// Create a function called shuffle that takes in an array as a parameter
+// remove the first item in the array so it's not included in the shuffled return array
+// Next using a for loop, assign each color in the array without first color an attached value to index
+// Return array newly shuffled
+
+ 
+const shuffle = (array) => {
+    array.shift()
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+}
+return array
+}
 
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
 
+
+describe("minMax", () => {
 const nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
 const nums2 = [109, 5, 9, 67, 8, 24]
 // Expected output: [5, 109]
+    it("a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () => {
+        expect(minMax(nums1)).toEqual([-8, 90])
+        expect(minMax(nums2)).toEqual([5, 109])
+    })
+})
+
+// ● minMax › a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order
+
+// ReferenceError: minMax is not defined
+
 
 
 // b) Create the function that makes the test pass.
+
+// Create a function called minMax with an array as the parameter
+// Use built-in method to sort the numbers from least to greatest
+// Return the 0th and last index in an array
+
+const minMax = (array) => {
+   array.sort((a,b) => a-b)
+   return [array[0],array[array.length -1]]
+}
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
+describe("uniqArray", () => {
 const testArray1 = [3, 7, 10, 5, 4, 3, 3]
 const testArray2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+    it("a function that takes in two arrays as arguments and returns one array with no duplicate values", () => {
+        expect(uniqArray(testArray1,testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+    })
+})
+
+// ReferenceError: uniqArray is not defined
 
 
 // b) Create the function that makes the test pass.
+
+
+// Create a function called uniqArray that takes in 2 arrays as parameter
+// create a variable to combine both arrays into one single array
+// Use a method to return a single array with only unique values
+
+const uniqArray = (array1, array2) => {
+    let combArray = array1.concat(array2)
+    let arraySet = new Set(combArray)
+    let finArray = Array.from(arraySet)
+    return finArray
+}
--- interview-questions.md
+++ interview-questions.md
@@ -7,52 +7,52 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are props in React?
 
-  Your answer:
+  Your answer: Props are properties of a class object in React. They can be used when referencing a property from another component. 
 
-  Researched answer:
+  Researched answer: Props allow us to pass data from a class-based component to another component in React. Props can be passed from the parent to children components and props are keys of the objects. The props can be accessed with this.props.(props name) syntax. When using props, you can set its value to more than just strings, but also numbers, booleans, arrays, variables, and objects.
 
 
 
 2. What is a DOM event?
 
-  Your answer:
+  Your answer: Document object model is what you see on the computer as a user. 
 
-  Researched answer:
+  Researched answer: DOM events are actions being done by the user and/or  by the computer. They can include click, resize, load, touch and others and are not limited to just place on the document. Events signal the occurence of something happening and can then be used in functions (onClick=). The mouse, keyboard strokes of a user can include DOM events like keydown, drag, click, doubleclick, drag, mouseover and more. 
 
 
 
 3. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: Object-oriented programming refers to isolating and storing information separately while still being able to be accessed and used as needed in coding. Functional coding relies more on functions for code to run. 
 
-  Researched answer:
+  Researched answer: Objects in programming deal with data that have information like fields, attributes, and methods. These classes or objects can be easily reused code. Functional programming on the other hand, is more math-based programming that are composed of functions. Functional programming is thought to be less likely to have bugs due to the fact that with some given arguments, the return is consistent. Object-oriented programming allows for more dynamic behavior to include inheritance, polymorphism, encapsulaiton. 
 
 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
-  Your answer:
+  Your answer: A float is a decimal number while an integer is a whole number in Ruby. They are both distinctyly their own type of data and have to be specified in the code. 
 
-  Researched answer:
+  Researched answer: An integer is similar to a natural number starting from 1, 2, 3, 4, and so forth with the difference being an integer also goes in the negative direction  as well: -1, -2, -3, etc. A float in Ruby represents inexact real numbers and contains a decimal point. Float is different from decimal in that decimal is used for more precise decimal numbers. 
 
 
 
 5. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: An implicit return means that without making specific instructions for how the code should return, it will automatically reference the last line of code. 
 
-  Researched answer:
+  Researched answer: Implicit return in Ruby means that the block of code will return the last line value without needing to include "return" whereas in Javascript without a return, it comes back undefined. 
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Instance Variable:
+1. Instance Variable: Instance variable is defined in a class that can be used by other objects that have instantiated the class. Ruby instance variable has the @ symbol as opposed to JS's this. 
 
-2. PostgreSQL:
+2. PostgreSQL: "Is an advanced version of SQL to provide support to different functions..." On Macs, it's the default db. 
 
-3. Ruby on Rails:
+3. Ruby on Rails: A server-side framework that allows database creation, migrations, and scaffoldig to assist in providing structure and build websites.
 
-4. ORM:
+4. ORM: Object-rational mapping converts data between relational dbs and object-oriented programming languages. It helps with efficiency of development time. 
 
-5. Active Record:
+5. Active Record: Active Record is an ORM. It can create models using generate command. 
